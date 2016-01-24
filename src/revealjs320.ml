5047b0d86d12b78eac079131c687192d6fe159b9
Richard Mortier <mort@cantab.net>
2016-01-23 17:24:15 -0800

    operator precedence! build correct print CSS URL

--- src/revealjs320.ml
+++ src/revealjs320.ml
@@ -39,9 +39,9 @@ let scripts ~revealv =
       var link = document.createElement('link');
       link.rel = 'stylesheet';
       link.type = 'text/css';
-      link.href = '/$str:revealv$/' +
-        window.location.search.match(/print-pdf/gi/) ?
-          'css/print/pdf.css' : 'css/print/paper.css';
+      link.href = '$str:revealv$/' +
+        (window.location.search.match(/print-pdf/gi) ?
+          'css/print/pdf.css' : 'css/print/paper.css');
       document.getElementsByTagName('head')[0].appendChild( link );
     </script>
   >>
